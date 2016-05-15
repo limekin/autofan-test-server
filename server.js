bc8656ac1bcbc98cddfa2ff1a00021dfc8316186
Ajay Shibu <ajay.shibu.ben@gmail.com>
2016-05-15 16:35:25 +0530

    Added fan-state_read api function at endpoint /state

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ var app = express();
 
 var autofanPort = 3000;
 
+var autofanStateVar = 0;
+
+app.get( '/state', function(req,res) {
+    res.send('{"state":"'+ autofanStateVar +'"}');
+});
+
 app.listen(autofanPort, function() {
-    console.log('Listening on port '+autofanPort+'.');
+    console.log('Listening on port '+ autofanPort +'.');
 });
