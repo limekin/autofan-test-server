14c6aa2a4ed49f9b23be04cb82d5e255373b496a
Ajay Shibu <ajay.shibu.ben@gmail.com>
2016-05-16 23:09:47 +0530

    Added 5 speed states along with UP DOWN and SHIFT actions

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ app.use(bodyParser.json());
 var autofanPort = 3000;
 
 var autofanStateVar = 0;
+var autofanStateVarPrev = 0;
+var autofanStateMax = 5;
 
 app.get( '/state', function(req,res) {
     res.send({ state: autofanStateVar });
@@ -20,18 +22,56 @@ app.post( '/action', function(req,res) {
     var reqAction = req.body.action;
     console.log(reqAction); //debug
 
-    if( reqAction == "ON" )
-	autofanStateVar = 1;
-    else if( reqAction == "OFF" )
-	autofanStateVar = 0;
-    else if( reqAction == "TOGGLE" ) {
-	
-	if(autofanStateVar == 0)
+    if( reqAction == "ON" ) {
+	if( !autofanStateVar ) {
 	    autofanStateVar = 1;
-	else if(autofanStateVar == 1)
-	    autofanStateVar = 0;
+	    autofanStateVarPrev = 0;
+	}
     }
-
+    else if( reqAction == "OFF" ) {
+	autofanStateVar = 0;
+	autofanStateVarPrev = 0;
+    }
+    else if( reqAction == "TOGGLE" ) {
+	var stateCache = autofanStateVar;
+	if( autofanStateVar ) {
+	    autofanStateVar = 0;
+	    autofanStateVarPrev = stateCache;
+	}
+	else {
+	    if( autofanStateVarPrev ) {
+		autofanStateVar = autofanStateVarPrev;
+		autofanStateVarPrev = stateCache;
+	    }
+	}
+    }
+    else if( reqAction == "UP" ) {
+	var stateCache = autofanStateVar;
+	if( (autofanStateVar) && (autofanStateVar < autofanStateMax) )
+	    autofanStateVar += 1;
+	autofanStateVarPrev = stateCache;
+    }
+    else if( reqAction == "DOWN" ) {
+	if( autofanStateVar > 1 ) {
+	    var stateCache = autofanStateVar;
+	    autofanStateVar -= 1;
+	    autofanStateVarPrev = stateCache;
+	}
+    }
+    else if( reqAction == "SHIFT" ) {
+	var toState = req.body.state;
+	if((autofanStateVar)
+	   && (toState)
+	   && (toState > 0)
+	   && (toState <= autofanStateMax)
+	   && (toState != autofanStateVar))
+	{
+	    var stateCache = autofanStateVar;
+	    autofanStateVar = toState;
+	    autofanStateVarPrev = stateCache;
+	}
+    }
+    
     res.send({ state: autofanStateVar });
 });
 	
