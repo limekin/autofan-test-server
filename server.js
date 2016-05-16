0e5c1c2db475d1f6ffa5bc241735884b3e9c6e3c
Ajay Shibu <ajay.shibu.ben@gmail.com>
2016-05-16 23:53:05 +0530

    Comments added for actions

--- server.js
+++ server.js
@@ -22,16 +22,19 @@ app.post( '/action', function(req,res) {
     var reqAction = req.body.action;
     console.log(reqAction); //debug
 
+    // ON action - Master switch; other commands active only after issuing this command
     if( reqAction == "ON" ) {
 	if( !autofanStateVar ) {
 	    autofanStateVar = 1;
 	    autofanStateVarPrev = 0;
 	}
     }
+    // OFF action - Ends the autofan session
     else if( reqAction == "OFF" ) {
 	autofanStateVar = 0;
 	autofanStateVarPrev = 0;
     }
+    // TOGGLE action - When autofan is active, it toggles autofan from off  to last state
     else if( reqAction == "TOGGLE" ) {
 	var stateCache = autofanStateVar;
 	if( autofanStateVar ) {
@@ -45,12 +48,14 @@ app.post( '/action', function(req,res) {
 	    }
 	}
     }
+    // UP action - When autofan active, ups the speed by one unit
     else if( reqAction == "UP" ) {
 	var stateCache = autofanStateVar;
 	if( (autofanStateVar) && (autofanStateVar < autofanStateMax) )
 	    autofanStateVar += 1;
 	autofanStateVarPrev = stateCache;
     }
+    // DOWN action - When autofan active, downs the speed by one unit
     else if( reqAction == "DOWN" ) {
 	if( autofanStateVar > 1 ) {
 	    var stateCache = autofanStateVar;
@@ -58,6 +63,7 @@ app.post( '/action', function(req,res) {
 	    autofanStateVarPrev = stateCache;
 	}
     }
+    // SHIFT action (requires "state" field in POST data) - When autofan active, shifts the speed to the state provided
     else if( reqAction == "SHIFT" ) {
 	var toState = req.body.state;
 	if((autofanStateVar)
