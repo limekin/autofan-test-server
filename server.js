2e0f14bf85456ada21685f8558b135a86977c329
Ajay Shibu <ajay.shibu.ben@gmail.com>
2016-05-21 20:17:52 +0530

    Add basic device discovery using UDP broadcast listener
    Instructions to utilize the feature:
    
    	- Send a UDP broadcast across the network to the derived broadcast IP address
    	- Content of the broadcast should be "AFDCRQ" without the quotes of course
    	- After sending, listen for a response which should arrive fast enough. Choose timeout accordingly.
    	- The response will include the autofan port that should be used for further communications
    	- Please derive the Device IP from the UDP meta info.

--- server.js
+++ server.js
@@ -1,8 +1,36 @@
 var express = require('express');
+var dgram = require('dgram');
 
 var app = express();
 
 var autofanPort = 3000;
+var autofanDiscoverPort = 3001;
+
+var broadcastListener = dgram.createSocket('udp4');
+
+broadcastListener.on('listening', function () {
+    var address = broadcastListener.address();
+    console.log('UDP Server listening on ' + address.address + ":" + address.port);
+});
+
+broadcastListener.on('message', function (message, remote) {
+    console.log(remote.address + ':' + remote.port +' - ' + message);
+
+    // Selectively respond to the sender's port with the autofan port
+    if( message=="AFDCRQ" ) {
+	var message = new Buffer(String(autofanPort));
+
+	var udpResponder = dgram.createSocket('udp4');
+	udpResponder.send(message, 0, message.length, remote.port, remote.address, function(err, bytes) {
+	    if (err)
+		throw err;
+	    console.log('UDP Response sent to ' + remote.address +':'+ remote.port);
+	    udpResponder.close();
+	});
+    }
+});
+
+broadcastListener.bind(autofanDiscoverPort, '');
 
 var autofanStateVar = 0;
 
